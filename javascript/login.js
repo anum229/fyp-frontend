16ebf4261fd5ed86988a19626157135ea11c541f
Muhammad Talha <talha.muhammad7980@gmail.com>
2025-07-28 21:40:46 +0500

    Testing base url

--- javascript/login.js
+++ javascript/login.js
@@ -36,7 +36,7 @@ async function validateLogin(requestedRole) {
     loginButton.disabled = true;
 
     try {
-        const response = await fetch("http://localhost:5000/api/auth/login", {
+        const response = await fetch("https://fyp-backend-8mc0.onrender.com/api/auth/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
